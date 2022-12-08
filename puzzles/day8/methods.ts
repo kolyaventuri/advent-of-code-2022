1cbb395d516158e84256ff4a4f1c390ed76e9b17
Kolya Venturi <kolya.venturi@enx3s.com>
2022-12-08 12:07:00 -0700

    More data testing - day 8

--- puzzles/day8/methods.ts
+++ puzzles/day8/methods.ts
@@ -1,59 +1,82 @@
 import { log } from "../../utils/logger";
 
-const generateGrid = (input: string): number[][] =>
+export const generateGrid = (input: string): number[][] =>
   input.split('\n').map(line =>
     line.split('').map(n => Number.parseInt(n, 10)));
 
+// Swaps rows for columns
+export const rotateGrid = <T>(grid: T[][]): T[][] => {
+  const flipped: typeof grid = new Array(grid[0].length).fill(new Array(grid.length));
+  for (let j = 0; j < grid[0].length; j++) {
+    const newRow: T[] = [];
+    for (let i = 0; i < grid[0].length; i++) {
+      const index = i % grid.length;
+      newRow.push(grid[index][j]);
+      //log(`[${index}][${j}] => [${j}][${index}] = ${grid[index][j]}`)
+    }
 
-type Direction = 'up' | 'down' | 'left' | 'right';
-const scanDirection = (trees: number[][], direction: Direction): boolean[][] => {
-  let dir: number;
-  switch (direction) {
-    case 'up':
-    case 'right':
-      dir = 1;
-      break;
-    case 'down':
-    case 'left':
-      dir = -1;
-      break;
+    flipped[j] = newRow;
   }
 
+  return flipped;
+}
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+export const calculateVisibility = (trees: number[][], direction: 'left' | 'right'): boolean[][] => {
   const visibility = new Array(trees.length).fill(new Array(trees[0].length));
-  if (direction === 'left' || direction === 'right') {
-    for (let i = 0; i < trees.length; i++) {
-      const row = trees[i];
-      const firstIndex = direction === 'left' ? 0 : row.length - 1;
-      const first = row[firstIndex];
-      visibility[i] = row.map((tree, index) => {
-        if (index === firstIndex) return true;
-        return tree - first > 0;
-      });
-    }
-  } else  {
-    const _trees = direction === 'up' ? [...trees] : [...trees].reverse();
-    for (let col = 0; col < _trees[0].length; col++) {
-      for (let row = 0; row < _trees.length; row++) {
-        log('Compare', _trees[row][col] to _trees[row][0])
-        visibility[row][col] = _trees[row][col] - _trees[row][0] > 0;
-      }
-    }
+
+  for (let i = 0; i < trees.length; i++) {
+    const row = trees[i];
+    const firstIndex = direction === 'left' ? 0 : row.length - 1;
+    const first = row[firstIndex];
+    visibility[i] = row.map((tree, index) => {
+      if (index === firstIndex) return true;
+      return tree - first > 0;
+    });
   }
 
   return visibility;
 };
+export const scanDirection = (trees: number[][], direction: Direction): boolean[][] => {
+  if (direction === 'left' || direction === 'right') {
+    return calculateVisibility(trees, direction);
+  }
 
-export const getVisibility = (trees: number[][]): boolean[][] => {
-  const visibility = new Array(trees.length).fill(new Array(trees[0].length));
+  const flipped = rotateGrid(trees); 
+  const result = calculateVisibility(flipped, direction === 'up' ? 'left' : 'right');
+
+  return rotateGrid(result);
+};
+
+const flatMerge = <T>(grids: Array<T[][]>): boolean[] => {
+  const flattened = grids.map(g => g.flat());
+  log(flattened);
+
+  let result: T[] = [];
+  for (const list of flattened) {
+    for (let i = 0; i < list.length; i++) {
+      result[i] ||= list[i];
+    }
+  }
+
+  return result.map(n => !!n);
+};
+
+export const getVisibility = (trees: number[][]): boolean[] => {
   const left = scanDirection(trees, 'left');
-  //log(left);
+  log('l', left);
   const right = scanDirection(trees, 'right');
-  //log(right);
+  log('r', right);
   
   const up = scanDirection(trees, 'up');
-  log(up);
+  log('u', up);
+  const down = scanDirection(trees, 'down');
+  log('d', down);
 
-  return visibility;
+  const merged = flatMerge([left, right, up, down]);
+  log('merged', merged);
+
+  return merged;
 };
 
 export const part1 = (input: string): number => {
@@ -62,8 +85,7 @@ export const part1 = (input: string): number => {
   const visible = getVisibility(trees);
   log(visible);
 
-  const flat = visible.flat();
-  const totalVisible: number = flat.map(n => (n ? 1 : 0) as number)
+  const totalVisible: number = visible.map(n => (n ? 1 : 0) as number)
     .reduce((s = 0, i) => s += i);
 
   log('total', totalVisible);
--- tests/day8.test.ts
+++ tests/day8.test.ts
@@ -1,13 +1,127 @@
 import test from 'ava';
-import {part1} from '../puzzles/day8/methods';
+import {calculateVisibility, getVisibility, scanDirection, part1, rotateGrid, generateGrid} from '../puzzles/day8/methods';
 
-const input =
+const testData =
 `30373
 25512
 65332
 33549
 35390`;
+
 test('(day8:part1) Returns the correct number of visible trees', t => {
-  const result = part1(input);
+  const result = part1(testData);
   t.is(result, 21);
+});
+
+test('(day8:helper) #rotateGrid can rotate a square grid', t => {
+  const input = [
+    [0, 1],
+    [2, 3]
+  ];
+  const expected = [
+    [0, 2],
+    [1, 3]
+  ];
+
+  t.deepEqual(rotateGrid(input), expected);
+});
+
+test('(day8:helper) #rotateGrid can rotate a square grid back', t => {
+  const input = [
+    [0, 1],
+    [2, 3]
+  ];
+  const expected = [
+    [0, 2],
+    [1, 3]
+  ];
+
+  t.deepEqual(rotateGrid(expected), input);
+});
+
+test('(day8:helper) #calculateVisibility can calculate left-facing visibility', t => {
+  const input = [
+    [0, 1],
+    [4, 3]
+  ];
+  const expected = [
+    [true, true],
+    [true, false]
+  ];
+
+  t.deepEqual(calculateVisibility(input, 'left'), expected);
+});
+
+test('(day8:helper) #calculateVisibility can calculate right-facing visibility', t => {
+  const input = [
+    [0, 1],
+    [4, 3]
+  ];
+  const expected = [
+    [false, true],
+    [true, true]
+  ];
+
+  t.deepEqual(calculateVisibility(input, 'right'), expected);
+});
+
+test('(day8:helper) #scanDirection can calculate up-facing visibility via rotation', t => {
+  const input = [
+    [0, 3],
+    [4, 3]
+  ];
+  const expected = [
+    [true, true],
+    [true, false]
+  ];
+
+  t.deepEqual(scanDirection(input, 'up'), expected);
+});
+
+test('(day8:helper) #scanDirection can calculate down-facing visibility via rotation', t => {
+  const input = [
+    [0, 3],
+    [4, 3]
+  ];
+  const expected = [
+    [false, false],
+    [true, true]
+  ];
+
+  t.deepEqual(scanDirection(input, 'down'), expected);
+});
+
+test('(day8:helper) #getVisibility calculates the total merged visiblity', t => {
+  const input = [
+    [0, 5, 4],
+    [4, 3, 5],
+    [3, 6, 4]
+  ];
+
+  const expected = [true, true, true, true, false, true, true, true, true];
+  t.deepEqual(getVisibility(input), expected);
+});
+
+test('(day8:helper) #generatedGrid reads in the grid correctly', t => {
+  const input = 
+`054
+435
+364`;
+  const expected = [
+    [0, 5, 4],
+    [4, 3, 5],
+    [3, 6, 4]
+  ];
+
+  t.deepEqual(generateGrid(input), expected);
+});
+
+test('(day8:part1) Works for a smaller data set', t => {
+const input = 
+`0547
+4356
+3847
+6932`;
+
+  t.is(part1(input), 14);
 });
